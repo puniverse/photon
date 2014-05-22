9a54b328776f2dd1843409df70b400eb384344e5
eitan <eitan@paralleluniverse.co>
2014-05-22 13:25:16 +0300

    use logger

--- src/main/java/co/paralleluniverse/photon/Photon.java
+++ src/main/java/co/paralleluniverse/photon/Photon.java
@@ -1,8 +1,8 @@
 package co.paralleluniverse.photon;
 
-import co.paralleluniverse.common.benchmark.StripedLongTimeSeries;
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.fibers.httpclient.FiberHttpClient;
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.google.common.util.concurrent.RateLimiter;
@@ -10,13 +10,17 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -28,24 +32,24 @@ import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.nio.reactor.IOReactorException;
 
 public class Photon {
-    private static final int TIMEOUT = 60000;
+    static final int TIMEOUT = 60000;
     static final int MAX_CONN = 150000;
     static final int DURATION = 20;
-    public static MetricRegistry metrics = new MetricRegistry();
+    static final MetricRegistry metrics = new MetricRegistry();
+    static final Log log = LogFactory.getLog(Photon.class);
 
     public static void main(String[] args) throws InterruptedException, IOReactorException, IOException {
         if (args.length < 2) {
             System.out.println("args are " + Arrays.toString(args));
             System.out.println("Usage: ClientTesters url rate");
             System.out.println("Example:\n\tClientTesters http://localhost:8080/regular?sleep=50 500");
-            args = new String[]{"http://www.google.com", "1"};
+            args = new String[]{"http://www.google.com", "3"};
 //            System.exit(0);
         }
         final ConcurrentHashMap<String, AtomicInteger> errors = new ConcurrentHashMap<>();
         final HttpGet request = new HttpGet(args[0]);
         final int rate = Integer.parseInt(args[1]);
-        System.out.println("configuration: " + request + " " + rate);
-        final ThreadFactory deamonTF = new ThreadFactoryBuilder().setDaemon(true).build();
+        log.info("configuration: " + request + " " + rate);
 
         try (CloseableHttpClient client = new FiberHttpClient(createDefaultHttpAsyncClient(null))) {
             System.out.println(new Date() + " starting..");
@@ -53,9 +57,11 @@ public class Photon {
             boolean print = true;
             int tenth = num / 10;
             Timer httpTimer = metrics.timer("httpTimer");
+            Meter requestMeter = metrics.meter("req");
+
             CountDownLatch cdl = new CountDownLatch(num);
             Semaphore sem = new Semaphore(MAX_CONN);
-            deamonTF.newThread(() -> {
+            new ThreadFactoryBuilder().setDaemon(true).build().newThread(() -> {
                 final RateLimiter rl = RateLimiter.create(rate);
 
                 for (int i = 0; i < num; i++) {
@@ -67,30 +73,32 @@ public class Photon {
                     final int constCounter = i + 1;
                     new Fiber<Void>(() -> {
                         final Timer.Context ctx = httpTimer.time();
-                        if (print && constCounter % tenth == 0)
-                            System.out.println(new Date() + " sent(" + constCounter + ") " + constCounter / tenth * 10 + "%..."
-                                    + " openConnections: " + (MAX_CONN - sem.availablePermits()));
+                        requestMeter.mark();
+                        if (constCounter % tenth == 0)
+                            log.info(" sent(" + constCounter + ") " + constCounter / tenth * 10 + "%..."
+                                    + " openConnections: " + (MAX_CONN - sem.availablePermits())
+                                    + " meanRate: " + df.format(requestMeter.getMeanRate()));
                         try {
                             client.execute(request).close();
                         } catch (IOException ex) {
                             errors.putIfAbsent(ex.getClass().getName(), new AtomicInteger());
                             errors.get(ex.getClass().getName()).incrementAndGet();
                         } finally {
+                            ctx.stop();
                             sem.release();
                             cdl.countDown();
-                            ctx.stop();
                             long count = num - cdl.getCount();
                             if (print && (count) % tenth == 0)
-                                System.out.print(new Date() + " responeded (" + count + ") " + ((count) / tenth * 10) + "%..."
-                                        + " mean: " + httpTimer.getSnapshot().getMean()
-                                        + " 95th: " + httpTimer.getSnapshot().get95thPercentile()
-                                        + " 99th: " + httpTimer.getSnapshot().get99thPercentile());
+                                log.info(" responeded (" + count + ") " + ((count) / tenth * 10) + "%..."
+                                        + " mean: " + nanos2secs(httpTimer.getSnapshot().getMean())
+                                        + " 95th: " + nanos2secs(httpTimer.getSnapshot().get95thPercentile())
+                                        + " 99th: " + nanos2secs(httpTimer.getSnapshot().get99thPercentile()));
                         }
                     }).start();
                 }
             }).start();
             cdl.await();
-            errors.entrySet().stream().forEach(p -> System.out.println(p.getKey() + " " + p.getValue()));
+            errors.entrySet().stream().forEach(p -> log.info(p.getKey() + " " + p.getValue()));
         }
     }
 
@@ -106,4 +114,9 @@ public class Photon {
                 setConnectionManager(mngr).
                 setDefaultRequestConfig(RequestConfig.custom().setLocalAddress(address).build()).build();
     }
+
+    static String nanos2secs(double nanos) {
+        return df.format(nanos / TimeUnit.SECONDS.toNanos(1));
+    }
+    static DecimalFormat df = new DecimalFormat("#.##");
 }
