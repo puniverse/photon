f755324b7b5f7e91d2455b97ab496174bb5b4d3a
eitan <eitan@paralleluniverse.co>
2014-05-22 16:49:41 +0300

    use log4j and capsule

--- src/main/java/co/paralleluniverse/photon/Photon.java
+++ src/main/java/co/paralleluniverse/photon/Photon.java
@@ -6,20 +6,19 @@ import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.google.common.util.concurrent.RateLimiter;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -29,90 +28,108 @@ import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.nio.reactor.IOReactorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Photon {
-    static final int TIMEOUT = 60000;
-    static final int MAX_CONN = 150000;
-    static final int DURATION = 20;
-    static final MetricRegistry metrics = new MetricRegistry();
-    static final Log log = LogFactory.getLog(Photon.class);
 
     public static void main(String[] args) throws InterruptedException, IOReactorException, IOException {
-        if (args.length < 2) {
-            System.out.println("args are " + Arrays.toString(args));
-            System.out.println("Usage: ClientTesters url rate");
-            System.out.println("Example:\n\tClientTesters http://localhost:8080/regular?sleep=50 500");
-            args = new String[]{"http://www.google.com", "3"};
-//            System.exit(0);
-        }
-        final ConcurrentHashMap<String, AtomicInteger> errors = new ConcurrentHashMap<>();
-        final HttpGet request = new HttpGet(args[0]);
-        final int rate = Integer.parseInt(args[1]);
-        log.info("configuration: " + request + " " + rate);
+        
+        Options options = new Options();
+        options.addOption("rate", true, "requests per second");
+        options.addOption("duration", true, "test duration in seconds");
+        options.addOption("maxconnections", true, "maximum number of open connections");
+        options.addOption("timeout", true, "connection and read timeout in millis");
+        options.addOption("help", false, "print help");
 
-        try (CloseableHttpClient client = new FiberHttpClient(createDefaultHttpAsyncClient(null))) {
-            System.out.println(new Date() + " starting..");
-            int num = DURATION * rate;
-            int tenth = num / 10;
-            Timer httpTimer = metrics.timer("httpTimer");
-            Meter requestMeter = metrics.meter("req");
+        try {
+            CommandLine cmd = new BasicParser().parse(options, args);
+            String[] ar = cmd.getArgs();
+            if (cmd.hasOption("help") || ar.length!=1) 
+                printUsageAndExit(options);
+            final String url = ar[0];            
+            final int timeout = Integer.parseInt(cmd.getOptionValue("timeout","10000"));
+            final int maxConnections = Integer.parseInt(cmd.getOptionValue("maxconnections","150000"));
+            final int duaration = Integer.parseInt(cmd.getOptionValue("duration","10"));
+            final int rate = Integer.parseInt(cmd.getOptionValue("rate","10"));
+            final MetricRegistry metrics = new MetricRegistry();
+            final Logger log = LoggerFactory.getLogger(Photon.class);
+            System.out.println();
 
-            CountDownLatch cdl = new CountDownLatch(num);
-            Semaphore sem = new Semaphore(MAX_CONN);
-            final RateLimiter rl = RateLimiter.create(rate);
+            final ConcurrentHashMap<String, AtomicInteger> errors = new ConcurrentHashMap<>();
+            final HttpGet request = new HttpGet(url);
+            log.info("url:"+url+" rate:"+rate+" duration:"+duaration+" maxconnections:"+maxConnections+", "+"timeout:"+timeout);
 
-            for (int i = 0; i < num; i++) {
-                rl.acquire();
-                if (sem.availablePermits() == 0)
-                    System.out.println(new Date() + " waiting...");
-                sem.acquireUninterruptibly();
+            PoolingNHttpClientConnectionManager mngr = new PoolingNHttpClientConnectionManager(new DefaultConnectingIOReactor(IOReactorConfig.custom().
+                    setConnectTimeout(timeout).
+                    setIoThreadCount(10).
+                    setSoTimeout(timeout).
+                    build()));
+            mngr.setDefaultMaxPerRoute(maxConnections);
+            mngr.setMaxTotal(maxConnections);
+            CloseableHttpAsyncClient ahc = HttpAsyncClientBuilder.create().
+                    setConnectionManager(mngr).
+                    setDefaultRequestConfig(RequestConfig.custom().setLocalAddress(null).build()).build();
 
-                final int constCounter = i + 1;
-                new Fiber<Void>(() -> {
-                    final Timer.Context ctx = httpTimer.time();
-                    requestMeter.mark();
-                    if (constCounter % tenth == 0)
-                        log.info(" sent(" + constCounter + ") " + constCounter / tenth * 10 + "%..."
-                                + " openConnections: " + (MAX_CONN - sem.availablePermits())
-                                + " meanRate: " + df.format(requestMeter.getMeanRate()));
-                    try {
-                        client.execute(request).close();
-                    } catch (IOException ex) {
-                        errors.putIfAbsent(ex.getClass().getName(), new AtomicInteger());
-                        errors.get(ex.getClass().getName()).incrementAndGet();
-                    } finally {
-                        ctx.stop();
-                        sem.release();
-                        cdl.countDown();
-                        long count = num - cdl.getCount();
-                        if (count % tenth == 0)
-                            log.info(" responeded (" + count + ") " + ((count) / tenth * 10) + "%..."
-                                    + " mean: " + nanos2secs(httpTimer.getSnapshot().getMean())
-                                    + " 95th: " + nanos2secs(httpTimer.getSnapshot().get95thPercentile())
-                                    + " 99th: " + nanos2secs(httpTimer.getSnapshot().get99thPercentile()));
-                    }
-                }).start();
+            try (CloseableHttpClient client = new FiberHttpClient(ahc)) {
+                log.info("starting..");
+                int num = duaration * rate;
+                int tenth = num / 10;
+                Timer httpTimer = metrics.timer("httpTimer");
+                Meter requestMeter = metrics.meter("req");
+
+                CountDownLatch cdl = new CountDownLatch(num);
+                Semaphore sem = new Semaphore(maxConnections);
+                final RateLimiter rl = RateLimiter.create(rate);
+
+                for (int i = 0; i < num; i++) {
+                    rl.acquire();
+                    if (sem.availablePermits() == 0)
+                        System.out.println(new Date() + " waiting...");
+                    sem.acquireUninterruptibly();
+
+                    final int constCounter = i + 1;
+                    new Fiber<Void>(() -> {
+                        final Timer.Context ctx = httpTimer.time();
+                        requestMeter.mark();
+                        if (constCounter % tenth == 0)
+                            log.info(" sent(" + constCounter + ") " + constCounter / tenth * 10 + "%..."
+                                    + " openConnections: " + (maxConnections - sem.availablePermits())
+                                    + " meanRate: " + df.format(requestMeter.getMeanRate()));
+                        try {
+                            client.execute(request).close();
+                        } catch (IOException ex) {
+                            errors.putIfAbsent(ex.getClass().getName(), new AtomicInteger());
+                            errors.get(ex.getClass().getName()).incrementAndGet();
+                        } finally {
+                            ctx.stop();
+                            sem.release();
+                            cdl.countDown();
+                            long count = num - cdl.getCount();
+                            if (count % tenth == 0)
+                                log.info(" responeded (" + count + ") " + ((count) / tenth * 10) + "%..."
+                                        + " mean: " + nanos2secs(httpTimer.getSnapshot().getMean())
+                                        + " 95th: " + nanos2secs(httpTimer.getSnapshot().get95thPercentile())
+                                        + " 99th: " + nanos2secs(httpTimer.getSnapshot().get99thPercentile()));
+                        }
+                    }).start();
+                }
+                cdl.await();
+                log.info("Errors: "+errors.values().stream().mapToInt(AtomicInteger::get).sum());
+                errors.entrySet().stream().forEach(p -> log.info(p.getKey() + " " + p.getValue()));
             }
-            cdl.await();
-            errors.entrySet().stream().forEach(p -> log.info(p.getKey() + " " + p.getValue()));
+        } catch (ParseException ex) {
+            System.err.println("Parsing failed.  Reason: " + ex.getMessage());
         }
     }
 
-    public static CloseableHttpAsyncClient createDefaultHttpAsyncClient(final InetAddress address) throws IOReactorException, UnknownHostException {
-        PoolingNHttpClientConnectionManager mngr = new PoolingNHttpClientConnectionManager(new DefaultConnectingIOReactor(IOReactorConfig.custom().
-                setConnectTimeout(TIMEOUT).
-                setIoThreadCount(10).
-                setSoTimeout(TIMEOUT).
-                build()));
-        mngr.setDefaultMaxPerRoute(MAX_CONN);
-        mngr.setMaxTotal(MAX_CONN);
-        return HttpAsyncClientBuilder.create().
-                setConnectionManager(mngr).
-                setDefaultRequestConfig(RequestConfig.custom().setLocalAddress(address).build()).build();
+    private static void printUsageAndExit(Options options) {
+        new HelpFormatter().printHelp("java -jar photon.jar [options] url", options);
+        System.exit(0);
     }
 
     static String nanos2secs(double nanos) {
-        return df.format(nanos / TimeUnit.SECONDS.toNanos(1));
+        return df.format(nanos / TimeUnit.SECONDS.toNanos(1))+"s";
     }
     static DecimalFormat df = new DecimalFormat("#.##");
 }
