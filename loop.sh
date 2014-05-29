3a434b9f888de0a919be32a91d38e0788bbea024
eitan <eitan@paralleluniverse.co>
2014-05-29 19:06:55 +0300

    delete old scripts

--- loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo > res.log
-for i in `seq 1 500`
-do
-	wget -t 1 --timeout=8 -q -O - http://localhost:8080/fiber\?sleep\=5000 >> res.log &
-done
--- postinstall.sh
+++
@@ -1,7 +0,0 @@
-cat <<EOF
-USAGE: 
-$ ./gradlew server
-or
-$ ./gradlew clients http://serveraddres:port /path duration rate
-Enjoy
-EOF
