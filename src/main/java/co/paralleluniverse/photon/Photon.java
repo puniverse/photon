7feae1e46f343bcaece49f3d965fdd07e9fcd947
circlespainter <fabio@paralleluniverse.co>
2015-02-16 14:34:16 +0200

    :art: Cleanup

--- src/main/java/co/paralleluniverse/photon/Photon.java
+++ src/main/java/co/paralleluniverse/photon/Photon.java
@@ -49,15 +49,14 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
-import org.apache.http.nio.reactor.IOReactorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Photon {
 
-    public static void main(String[] args) throws InterruptedException, IOReactorException, IOException {
+    public static void main(final String[] args) throws InterruptedException, IOException {
 
-        Options options = new Options();
+        final Options options = new Options();
         options.addOption("rate", true, "requests per second");
         options.addOption("duration", true, "test duration in seconds");
         options.addOption("maxconnections", true, "maximum number of open connections");
@@ -69,26 +68,34 @@ public class Photon {
         options.addOption("help", false, "print help");
 
         try {
-            CommandLine cmd = new BasicParser().parse(options, args);
-            String[] ar = cmd.getArgs();
+            final CommandLine cmd = new BasicParser().parse(options, args);
+            final String[] ar = cmd.getArgs();
             if (cmd.hasOption("help") || ar.length != 1)
                 printUsageAndExit(options);
             final String url = ar[0];
             final int timeout = Integer.parseInt(cmd.getOptionValue("timeout", "10000"));
             final int maxConnections = Integer.parseInt(cmd.getOptionValue("maxconnections", "150000"));
-            final int duaration = Integer.parseInt(cmd.getOptionValue("duration", "10"));
+            final int duration = Integer.parseInt(cmd.getOptionValue("duration", "10"));
             final int printCycle = Integer.parseInt(cmd.getOptionValue("print", "1000"));
             final String testName = cmd.getOptionValue("name", "test");
             final int rate = Integer.parseInt(cmd.getOptionValue("rate", "10"));
             final MetricRegistry metrics = new MetricRegistry();
-            final Meter requestMeter = metrics.meter("reqest");
+            final Meter requestMeter = metrics.meter("request");
             final Meter responseMeter = metrics.meter("response");
             final Meter errorsMeter = metrics.meter("errors");
             final Logger log = LoggerFactory.getLogger(Photon.class);
             final ConcurrentHashMap<String, AtomicInteger> errors = new ConcurrentHashMap<>();
             final HttpGet request = new HttpGet(url);
-            final StripedTimeSeries<Long> sts = new StripedTimeSeries(30000, false);
+            final StripedTimeSeries<Long> sts = new StripedTimeSeries<>(30000, false);
             final StripedHistogram sh = new StripedHistogram(60000, 5);
+
+            log.info("name: "+testName+" url:" + url + " rate:" + rate + " duration:" + duration + " maxconnections:" + maxConnections + ", " + "timeout:" + timeout);
+            final DefaultConnectingIOReactor ioreactor = new DefaultConnectingIOReactor(IOReactorConfig.custom().
+                    setConnectTimeout(timeout).
+                    setIoThreadCount(10).
+                    setSoTimeout(timeout).
+                    build());
+
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 if (cmd.hasOption("stats"))
                     printFinishStatistics(errorsMeter, sts, sh, testName);
@@ -96,29 +103,23 @@ public class Photon {
                     errors.entrySet().stream().forEach(p -> log.info(testName+" "+p.getKey() + " " + p.getValue()+"ms"));
                 System.out.println(testName+" responseTime(90%): " + sh.getHistogramData().getValueAtPercentile(90)+"ms");
                 if (cmd.hasOption("minmax")) {
-                    HistogramData hd = sh.getHistogramData();
+                    final HistogramData hd = sh.getHistogramData();
                     System.out.format("%s %8s%8s%8s%8s\n", testName, "min", "mean", "sd", "max");
                     System.out.format("%s %8d%8.2f%8.2f%8d\n", testName, hd.getMinValue(), hd.getMean(), hd.getStdDeviation(), hd.getMaxValue());
                 }
             }));
 
-            log.info("name: "+testName+" url:" + url + " rate:" + rate + " duration:" + duaration + " maxconnections:" + maxConnections + ", " + "timeout:" + timeout);
-            PoolingNHttpClientConnectionManager mngr = new PoolingNHttpClientConnectionManager(new DefaultConnectingIOReactor(IOReactorConfig.custom().
-                    setConnectTimeout(timeout).
-                    setIoThreadCount(10).
-                    setSoTimeout(timeout).
-                    build()));
+            final PoolingNHttpClientConnectionManager mngr = new PoolingNHttpClientConnectionManager(ioreactor);
             mngr.setDefaultMaxPerRoute(maxConnections);
             mngr.setMaxTotal(maxConnections);
-            CloseableHttpAsyncClient ahc = HttpAsyncClientBuilder.create().
+            final CloseableHttpAsyncClient ahc = HttpAsyncClientBuilder.create().
                     setConnectionManager(mngr).
                     setDefaultRequestConfig(RequestConfig.custom().setLocalAddress(null).build()).build();
+            try (final CloseableHttpClient client = new FiberHttpClient(ahc)) {
+                final int num = duration * rate;
 
-            try (CloseableHttpClient client = new FiberHttpClient(ahc)) {
-                int num = duaration * rate;
-
-                CountDownLatch cdl = new CountDownLatch(num);
-                Semaphore sem = new Semaphore(maxConnections);
+                final CountDownLatch cdl = new CountDownLatch(num);
+                final Semaphore sem = new Semaphore(maxConnections);
                 final RateLimiter rl = RateLimiter.create(rate);
 
                 spawnStatisticsThread(printCycle, cdl, log, requestMeter, responseMeter, errorsMeter, testName);
@@ -131,7 +132,7 @@ public class Photon {
 
                     new Fiber<Void>(() -> {
                         requestMeter.mark();
-                        long start = System.nanoTime();
+                        final long start = System.nanoTime();
                         try {
                             client.execute(request).close();
                             responseMeter.mark();
@@ -141,7 +142,7 @@ public class Photon {
                             errors.get(ex.getClass().getName()).incrementAndGet();
                         } finally {
                             final long now = System.nanoTime();
-                            long millis = TimeUnit.NANOSECONDS.toMillis(now - start);
+                            final long millis = TimeUnit.NANOSECONDS.toMillis(now - start);
                             sts.record(start, millis);
                             sh.recordValue(millis);
                             sem.release();
@@ -150,45 +151,43 @@ public class Photon {
                     }).start();
                 }
                 cdl.await();
-
-//                printFinishStatistics(sts, sh, errorsMeter, errors, log, testName);
             }
-        } catch (ParseException ex) {
+        } catch (final ParseException ex) {
             System.err.println("Parsing failed.  Reason: " + ex.getMessage());
         }
     }
 
-    private static void printFinishStatistics(Meter errors, StripedTimeSeries<Long> sts, StripedHistogram sh, String testName) {
-        File file = new File(testName + ".txt");
+    private static void printFinishStatistics(final Meter errors, final StripedTimeSeries<Long> sts, final StripedHistogram sh, final String testName) {
+        final File file = new File(testName + ".txt");
         try (PrintWriter out = new PrintWriter(file)) {
             out.println("ErrorsCounter: "+errors.getCount());
-            long millisTime = new Date().getTime();
-            long nanoTime = System.nanoTime();
+            final long millisTime = new Date().getTime();
+            final long nanoTime = System.nanoTime();
             sts.getRecords().forEach(rec -> out.println(df.format(new Date(millisTime - TimeUnit.NANOSECONDS.toMillis(nanoTime - rec.timestamp)))
                     + " " + testName + " responseTime " + rec.value + "ms"));
             out.println("\nHistogram:");
             for (int i = 0; i <= 100; i++)
                 out.println(testName + " responseTimeHistogram " + i + "% : " + sh.getHistogramData().getValueAtPercentile(i));
-            System.out.println("staticsfile: "+file.getAbsolutePath());
-        } catch (FileNotFoundException ex) {
-            System.err.println(ex);
+            System.out.println("Statistics file: " + file.getAbsolutePath());
+        } catch (final FileNotFoundException ex) {
+            System.err.println(ex.getMessage());
         }
     }
 
-    private static void spawnStatisticsThread(final int printCycle, CountDownLatch cdl, final Logger log, Meter requestMeter, Meter responseMeter, Meter errorsMeter, final String testName) {
+    private static void spawnStatisticsThread(final int printCycle, CountDownLatch cdl, final Logger log, final Meter requestMeter, final Meter responseMeter, final Meter errorsMeter, final String testName) {
         new Thread(() -> {
             try {
                 if (printCycle > 0)
                     while (!cdl.await(printCycle, TimeUnit.MILLISECONDS)) {
                         printStatisticsLine(log, requestMeter, responseMeter, errorsMeter, testName);
                     }
-            } catch (InterruptedException ex) {
+            } catch (final InterruptedException ex) {
                 throw new RuntimeException(ex);
             }
         }).start();
     }
 
-    private static void printStatisticsLine(final Logger log, Meter requestMeter, Meter responseMeter, Meter errorsMeter, final String testName) {
+    private static void printStatisticsLine(final Logger log, final Meter requestMeter, final Meter responseMeter, final Meter errorsMeter, final String testName) {
         log.info(testName + " STATS: "
                 + "req: " + requestMeter.getCount() + " " + nf.format(requestMeter.getMeanRate()) + "Hz "
                 + "resp: " + responseMeter.getCount() + " " + nf.format(responseMeter.getMeanRate()) + "Hz "
@@ -196,14 +195,11 @@ public class Photon {
                 + "open: " + (requestMeter.getCount() - errorsMeter.getCount() - responseMeter.getCount()));
     }
 
-    private static void printUsageAndExit(Options options) {
+    private static void printUsageAndExit(final Options options) {
         new HelpFormatter().printHelp("java -jar photon.jar [options] url", options);
         System.exit(0);
     }
 
-    static String nanos2secs(double nanos) {
-        return nf.format(nanos / TimeUnit.SECONDS.toNanos(1)) + "s";
-    }
-    static SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
-    static DecimalFormat nf = new DecimalFormat("#.##");
+    private static SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss.SSS");
+    private static DecimalFormat nf = new DecimalFormat("#.##");
 }
